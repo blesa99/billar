87a5f8a5593fef67fc980ca019249775f280788e
blesa99 <sergiohijo1999@hotmail.com>
2024-05-21 01:34:00 +0200

    Configuración escena, cambio en GameSetup y añadir CameraController
    Hemos cambiado el taco para que sea hijo de la cámara y hemos seteado la cámara a encima del taco.
    Hemos añadido el script CameraControl con la lógica de la cámara y luego hemos cambiado el método Start del GameSetup por el método Awake para evitar crasheos posteriores

--- Billar3D/Assets/Scripts/GameSetup.cs
+++ Billar3D/Assets/Scripts/GameSetup.cs
@@ -15,7 +15,7 @@ public class GameSetup : MonoBehaviour
     [SerializeField] Transform headBallPosition;
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         ballRadius = ballPrefab.GetComponent<SphereCollider>().radius * 100f;
         ballDiameter = ballRadius * 2f;
